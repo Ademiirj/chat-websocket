034e48c015e93102467df4d940e296a44dc24d15
Giovanni Sena <@giovanni.sena>
2022-04-04 00:25:54 -0300

    config app listen

--- src/server.js
+++ src/server.js
@@ -13,9 +13,9 @@ app.set('views', path.join(__dirname, 'public'));
 app.engine('html', require('ejs').renderFile);
 app.set('view engine', 'html');
 
-app.use('/', (req, res)=>{
-    res.render('index.html');
-});
+app.listen(3000, function(){
+    console.log("Express server listening on port %d in %s mode", this.address().port);
+  });
 
 
 //array de messages vai servir para guardar nossas mensagens
